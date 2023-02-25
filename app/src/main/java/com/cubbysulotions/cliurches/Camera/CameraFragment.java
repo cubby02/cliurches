3f78874e62c37cc846daf94f07dd720b46480916
KonoDioDa <cthulhu3030@gmail.com>
2023-02-25 15:32:28 +0800

    camera functionality

--- app/src/main/java/com/cubbysulotions/cliurches/Camera/CameraFragment.java
+++ app/src/main/java/com/cubbysulotions/cliurches/Camera/CameraFragment.java
@@ -1,17 +1,31 @@
 package com.cubbysulotions.cliurches.Camera;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -20,6 +34,12 @@ import com.cubbysulotions.cliurches.Home.HomeActivity;
 import com.cubbysulotions.cliurches.R;
 
 import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class CameraFragment extends Fragment {
@@ -34,7 +54,16 @@ public class CameraFragment extends Fragment {
 
     private NavController navController;
     private ImageView imgSearch;
-    private Button btnSnapPhoto, btnOpenGallery;
+    private Button btnCamera, btnGallery, btnIdentify;
+    private Bitmap imageBitmap;
+    private final int REQUEST_IMAGE_CAPTURE = 1;
+    private final int IMAGE_PICK_CODE = 1000;
+    private final int REQUEST_WRITE_STORAGE = 2;
+    private String u_plantID; // unique plant id
+    private Uri contentUri;
+    private String url;
+    private String currentPhotoPath;
+    private Uri photoURI;
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -42,34 +71,78 @@ public class CameraFragment extends Fragment {
 
         navController = Navigation.findNavController(view);
         imgSearch = view.findViewById(R.id.imgSearchChurch);
-        btnSnapPhoto = view.findViewById(R.id.btnSnap);
-        btnOpenGallery = view.findViewById(R.id.btnGallery);
+        btnCamera = view.findViewById(R.id.btnSnap);
+        btnGallery = view.findViewById(R.id.btnGallery);
+        btnIdentify = view.findViewById(R.id.btnIdentify);
 
         snapPhoto();
         openGallery();
+        identify();
     }
 
-    private void openGallery() {
+    private void identify() {
+        btnIdentify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                identifyChurch();
+            }
+        });
+    }
+
+    private void identifyChurch() {
         try {
-            // TODO: add open gallery here
+//        String value = contentUri.toString();
+//        Intent intent = new Intent(getActivity(), .class);
+//        intent.putExtra("userPic", value);
+//        intent.putExtra("url", url);
+//        startActivity(intent);
+            // TODO: add take picture code here
 
-
+            navController.navigate(R.id.action_cameraFragment_to_matchResultFragment);
+            ((HomeActivity)getActivity()).hideNavigationBar(true);
+            ((HomeActivity)getActivity()).hideTopBarPanel(true);
         } catch (Exception e){
             toast("Something went wrong, please try again");
             Log.e(TAG, "onClick: ", e);
         }
+
+    }
+
+    private void openGallery() {
+        btnGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    // TODO: add open gallery here
+                    if(checkGalleryPermissions()){
+                        openingGallery();
+                    }else{
+                        requestGalleryPermission();
+                    }
+
+                } catch (Exception e){
+                    toast("Something went wrong, please try again");
+                    Log.e(TAG, "onClick: ", e);
+                }
+            }
+        });
+
     }
 
     private void snapPhoto() {
-        btnSnapPhoto.setOnClickListener(new View.OnClickListener() {
+        btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try {
                     // TODO: add take picture code here
-
-                    navController.navigate(R.id.action_cameraFragment_to_matchResultFragment);
-                    ((HomeActivity)getActivity()).hideNavigationBar(true);
-                    ((HomeActivity)getActivity()).hideTopBarPanel(true);
+                    if(checkCamPermissions()){
+                        takePicture();
+                    }else{
+                        requestCamPermission();
+                    }
+//                    navController.navigate(R.id.action_cameraFragment_to_matchResultFragment);
+//                    ((HomeActivity)getActivity()).hideNavigationBar(true);
+//                    ((HomeActivity)getActivity()).hideTopBarPanel(true);
                 } catch (Exception e){
                     toast("Something went wrong, please try again");
                     Log.e(TAG, "onClick: ", e);
@@ -77,6 +150,191 @@ public class CameraFragment extends Fragment {
             }
         });
     }
+
+
+    private void openingGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, IMAGE_PICK_CODE);
+    }
+    // when allowed app permissions
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+//            File f = new File(currentPhotoPath);
+            contentUri = photoURI;
+            imgSearch.setImageBitmap(setPic());
+//            saveImage(setPic());
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            String imageFileName = "IMAGE_" + timeStamp + "." + getFileExt(contentUri);
+
+        }
+        if(requestCode == IMAGE_PICK_CODE && resultCode == Activity.RESULT_OK){
+            contentUri = data.getData();
+            imgSearch.setImageURI(contentUri);
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            String imageFileName = "IMAGE_" + timeStamp + "." + getFileExt(contentUri);
+
+        }
+        if(requestCode == REQUEST_WRITE_STORAGE && resultCode == Activity.RESULT_OK){
+            //saveImage(imageBitmap);
+        }
+    }
+
+    //for file extension
+    private String getFileExt(Uri contentUri){
+        ContentResolver contentResolver = getActivity().getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(contentResolver.getType(contentUri));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch(requestCode) {
+            case REQUEST_IMAGE_CAPTURE: {
+                if (grantResults.length > 0) {
+                    boolean cameraPermission = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if(cameraPermission){
+                        toast("Permissions Granted..");
+                        takePicture();
+                    }else{
+                        toast("Permissions denied..");
+                    }
+                }
+            }
+            case IMAGE_PICK_CODE: {
+                if (grantResults.length > 0) {
+                    boolean galleryPermission = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if(galleryPermission){
+                        toast("Permissions Granted..");
+                        openGallery();
+                    }else{
+                        toast("Permissions denied..");
+                    }
+                }
+            }
+            case REQUEST_WRITE_STORAGE: {
+                if (grantResults.length > 0) {
+                    boolean storagePermission = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if(storagePermission){
+                        toast("Permissions Granted..");
+                    }else{
+                        toast("Permissions denied..");
+                    }
+                }
+            }
+        }
+    }
+
+
+    private void takePicture() {
+        dispatchTakePictureIntent();
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if(checkStoragePermissions()){
+            if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+                // Create the File where the photo should go
+                File photoFile = null;
+                try {
+                    photoFile = createImageFile();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                    // Error occurred while creating the File
+                }
+                // Continue only if the File was successfully created
+                if (photoFile != null) {
+                    photoURI = FileProvider.getUriForFile(getActivity(),
+                            "com.example.CLIURCHES",
+                            photoFile);
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        }
+        else{
+            requestStoragePermission();
+        }
+    }
+
+    // creating root img file  (only for the application and cannot be seen in gallery)
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "IMAGES_" + timeStamp + "_";
+//        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private boolean checkStoragePermissions(){
+        int camPermission = ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        return camPermission == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestStoragePermission(){
+        int PERMISSION_CODE = 201;
+        requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_CODE);
+    }
+    private boolean checkGalleryPermissions(){
+        int camPermission = ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.READ_EXTERNAL_STORAGE);
+        return camPermission == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestGalleryPermission(){
+        int PERMISSION_CODE = 1001;
+        requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_CODE);
+    }
+    
+    private boolean checkCamPermissions(){
+        int camPermission = ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA);
+        return camPermission == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestCamPermission(){
+        int PERMISSION_CODE = 200;
+        requestPermissions(new String[]{Manifest.permission.CAMERA}, PERMISSION_CODE);
+    }
+
+
+    // the bitmap displayed in the imageView
+    private Bitmap setPic() {
+        // Get the dimensions of the View
+        int targetW = imgSearch.getWidth();
+        int targetH = imgSearch.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+
+        BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.max(1, Math.min(photoW/targetW, photoH/targetH));
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+        Bitmap bitmap = BitmapFactory.decodeFile(currentPhotoPath, bmOptions);
+
+        btnIdentify.setVisibility(View.VISIBLE);
+        return bitmap;
+    }
+
 
     private void toast(String msg ){
         Toast.makeText(getActivity(), msg, Toast.LENGTH_LONG).show();
