e56ef706569c465793edb87998d37342594aeb49
NJ <noeljustine.mendoza@gmail.com>
2023-03-04 03:12:30 +0800

    map

--- app/src/main/java/com/cubbysulotions/cliurches/Home/SettingsBottomSheetDialog.java
+++ app/src/main/java/com/cubbysulotions/cliurches/Home/SettingsBottomSheetDialog.java
@@ -302,7 +302,7 @@ public class SettingsBottomSheetDialog extends BottomSheetDialogFragment {
                         public void onClick(DialogInterface dialog, int which) {
                             switch (which){
                                 case DialogInterface.BUTTON_POSITIVE:
-                                    logout();
+                                    logoutSession();
                                     break;
                                 case DialogInterface.BUTTON_NEGATIVE:
 
